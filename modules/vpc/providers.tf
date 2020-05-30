3274a1f412482bd773fbe5261db09ec7cf0ad681
veerendradevops <veerendratholeti@gmail.com>
2020-05-31 04:22:27 +0530

    sdfa

--- modules/vpc/providers.tf
+++ modules/vpc/providers.tf
@@ -1,6 +1,8 @@
 provider "aws" {
+    region = "us-east-1"
   assume_role {
     role_arn     = "arn:aws:iam::911348902258:role/vkterraform_role"
+    profile      = "default"
     #session_name = "SESSION_NAME"
     #external_id  = "EXTERNAL_ID"
   }
