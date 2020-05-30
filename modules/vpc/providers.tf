712886937fc6f0ffeaddd79ffbb15e297617e238
veerendradevops <veerendratholeti@gmail.com>
2020-05-31 04:46:37 +0530

    sdfa

--- modules/vpc/providers.tf
+++ modules/vpc/providers.tf
@@ -1,10 +1,6 @@
 provider "aws" {
     region = "us-east-1"
-  assume_role {
+
     shared_credentials_file = "~/.aws/credentails"
     profile                 = "default"
-    #role_arn     = "arn:aws:iam::<account>:role/vkssmrole"
-    #session_name = "SESSION_NAME"
-    #external_id  = "EXTERNAL_ID"
-  }
 }
