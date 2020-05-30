e0c6a0a75aff4bb622a79e71cb8c9ab3be532e64
veerendradevops <veerendratholeti@gmail.com>
2020-05-31 04:33:20 +0530

    sdfa

--- modules/vpc/providers.tf
+++ modules/vpc/providers.tf
@@ -1,7 +1,9 @@
 provider "aws" {
     region = "us-east-1"
   assume_role {
-    role_arn     = "arn:aws:iam::911348902258:role/vkssmrole"
+    shared_credentials_file = "/home/ubuntu/.aws/credentails"
+      profile                 = "default"
+    #role_arn     = "arn:aws:iam::<account>:role/vkssmrole"
     #session_name = "SESSION_NAME"
     #external_id  = "EXTERNAL_ID"
   }
