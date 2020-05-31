4c44f2a27a67eb901325e6be98967456ba5e8cab
veerendradevops <veerendratholeti@gmail.com>
2020-05-31 20:44:28 +0530

    added

--- VPC/backend.tf
+++
@@ -1,8 +0,0 @@
-backend "s3" {
- encrypt = true
- bucket = "backendvk"
- dynamodb_table = "tfdynamo-state-lock"
- region = "us-east-1"
- key = "terraform.tfstate"
- }
-}
--- VPC/main.tf
+++ VPC/main.tf
@@ -2,5 +2,12 @@ module "vpc" {
     source ="../modules/vpc"
     cidr_block = "${var.cidr_block}"
     availability_zones = "${var.availability_zones}"
-
+backend "s3" {
+ encrypt = true
+ bucket = "backendvk"
+ dynamodb_table = "tfdynamo-state-lock"
+ region = "us-east-1"
+ key = "terraform.tfstate"
+ }
+}
 }
