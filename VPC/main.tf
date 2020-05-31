5affb04f9eb24149b1084bed7251e444d22e9ac3
veerendradevops <veerendratholeti@gmail.com>
2020-06-01 00:52:38 +0530

    added

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,6 +1,22 @@
+module s3_backend {
+  source                      = "youngfeldt/backend-s3/aws"
+  version                     = ">= 1.0.0"
+  backend_s3_bucket           = "backendvk"
+  backend_dynamodb_lock_table = "tfdynamo-state-lock"
+  create_dynamodb_lock_table  = true
+  create_s3_bucket            = true
+  s3_key                      = "terraform.tfstate"
+}
+
 module "vpc" {
     source ="../modules/vpc"
     cidr_block = "${var.cidr_block}"
     availability_zones = "${var.availability_zones}"
 
+}
+terraform {
+    backend "s3"{
+        bucket = "backendvk"
+        key = "terraform.tfstate"
+    }
 }
