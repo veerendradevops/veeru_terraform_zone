62c676dde5e23eaf5688f78daaf89be6966ce576
veerendradevops <veerendratholeti@gmail.com>
2020-05-31 18:16:41 +0530

    added

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -22,3 +22,13 @@ resource "aws_subnet" "vkwebsubnets" {
   }
 }
 
+resource "aws_subnet" "vkprivatesubnets" {
+  vpc_id     = "${aws_vpc.TerraformVPC1.id}"
+  cidr_block = "${cidrsubnet(var.cidr_block, 8, count.index + length(var.availability_zones))}"
+  count      = "${length(var.availability_zones)}"
+  availability_zone = "${element(var.availability_zones, count.index )}"
+
+  tags = {
+    "Name" = "vkprivatesubnet - ${element(var.availability_zones, count.index)}"
+  }
+}
