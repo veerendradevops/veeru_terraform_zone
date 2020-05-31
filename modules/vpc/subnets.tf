c487166370ca00b448a93f6fc0c25ff40caf3cda
veerendradevops <veerendratholeti@gmail.com>
2020-05-31 18:09:31 +0530

    added

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -1,10 +1,24 @@
-resource "aws_subnet" "vksubnets" {
+#Note: cidrsubnet(), element() are terraform functions
+
+resource "aws_subnet" "vkdmzsubnets" {
   vpc_id     = "${aws_vpc.TerraformVPC1.id}"
   cidr_block = "${cidrsubnet(var.cidr_block, 8, count.index)}"
   count      = "${length(var.availability_zones)}"
   availability_zone = "${element(var.availability_zones, count.index)}"
 
   tags = {
-    "Name" = "vktfsubnet - ${element(var.availability_zones, count.index)}"
+    "Name" = "vkpublicsubnet - ${element(var.availability_zones, count.index)}"
   }
 }
+
+resource "aws_subnet" "vkwebsubnets" {
+  vpc_id     = "${aws_vpc.TerraformVPC1.id}"
+  cidr_block = "${cidrsubnet(var.cidr_block, 8, count.index)}"
+  count      = "${length(var.availability_zones)}"
+  availability_zone = "${element(var.availability_zones, count.index)}"
+
+  tags = {
+    "Name" = "vknatsubnet - ${element(var.availability_zones, count.index)}"
+  }
+}
+
