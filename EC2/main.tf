b2c2721c301bac4b6ab5f763f21c15cfa719c0c0
veerendradevops <veerendratholeti@gmail.com>
2020-06-01 20:07:31 +0530

    import

--- EC2/main.tf
+++ EC2/main.tf
@@ -6,3 +6,12 @@ key_name = "veeru7799"
 availability_zones = "us-east-1a"
 region = "us-east-1"
 }
+
+data "aws_instance" "data_apache" {
+  instance_id = "i-00a4f3af7a5d682ea"
+
+  filter {
+    name   = "tag:Name"
+    values = ["apache"]
+  }
+}
