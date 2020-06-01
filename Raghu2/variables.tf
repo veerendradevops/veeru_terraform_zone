53b942eca3b506cef1c4db9dafaf78d5967f05e1
veerendradevops <veerendratholeti@gmail.com>
2020-06-02 00:57:53 +0530

    added

--- Raghu2/variables.tf
+++ Raghu2/variables.tf
@@ -1,21 +1,26 @@
 variable "region" {
     description = "region to create instance"
+    default = "us-east-1"
 }
 
 variable "ami" {
 description = "image id to launch"
+default = "ami-09d95fab7fff3776c"
 }
 
 variable "key_name"{
 description = "keyname"
+default = "veeru7799"
 }
 
 variable "instance_type" {
 description = "instancetype to launch"
+default = "t2.micro"
 }
 
 variable "availability_zones" {
 description = "Availabilityzones that are going to be used for the subnets"
+default = "us-east-1a"
 }
 
 ####Module Parameters ####
