7fe2b5217ad87794bc77edba776d71d2358cb29a
veerendradevops <veerendratholeti@gmail.com>
2020-06-02 00:40:57 +0530

    sdfa

--- Raghu/modules/ec2.tf
+++ Raghu/modules/ec2.tf
@@ -9,3 +9,29 @@ resource "aws_instance" "terraformmachine" {
     Name      = "${var.environment_tag}"
     }
 }
+
+data "aws_instance" "data_apache" {
+  instance_id = "${aws_instance.terraformmachine.id}"
+
+  filter {
+    name   = "tag:Name"
+    values = ["apache"]
+  }
+}
+
+resource "null_resource" "null_apache" {
+    connection {
+        user = "ec2-user"
+        host = "${data.aws_instance.data_apache.public_ip}"
+        private_key = "${file(var.privatekeypath)}"
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum update -y",
+            "sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm",
+            "sudo yum install epel-release-latest-7.noarch.rpm -y",
+            "sudo yum update -y",
+            "sudo  yum install git python python-devel python-pip ansible -y"
+        ]
+    }
+}
