35c0cc72c54a17d0c119fbcdf19c3403f36cee20
veerendradevops <veerendratholeti@gmail.com>
2020-06-02 04:24:17 +0530

    asdfsfd

--- Raghu/main.tf
+++
@@ -1,9 +0,0 @@
-module "ec2" {
-source = "../modules/ec2_module"
-ami = "ami-09d95fab7fff3776c"
-instance_type = "t2.micro"
-key_name = "veeru7799"
-availability_zones = "us-east-1a"
-region = "us-east-1"
-}
-
