0b2007eec9da960c945f824062b7e7b82a7f1a17
veerendradevops <veerendratholeti@gmail.com>
2020-06-01 20:50:53 +0530

    added

--- EC2/variables.tf
+++ EC2/variables.tf
@@ -1,3 +1,14 @@
+variable "region" {
+description = "Region"
+default = "us-east-1"
+}
+
+variable "privatekeypath" {
+description = "private pem file"
+default = "veeru7799.pem"
+}
+
+
 /*
 #global parameters like accesskey, secretket, region and availabilityzone etc
 
