d89af7fbf8732a8768c94c5d786672cfd77e0c4c
veerendradevops <veerendratholeti@gmail.com>
2020-06-02 00:14:05 +0530

    fs

--- EC2/main.tf
+++ EC2/main.tf
@@ -7,3 +7,29 @@ availability_zones = "us-east-1a"
 region = "us-east-1"
 }
 ####Note: To use below datasources & null resource, you need to create a ec2 machine using above code, else you may get an error #####
+
+data "aws_instance" "data_apache" {
+  instance_id = "${var.instance_id}"
+
+  filter {
+    name   = "tag:Name"
+    values = ["apache"]
+  }
+}
+
+resource "null_resource" "null_apache" {
+    connection {
+        user = "ec2-user"
+        host = "${data.aws_instance.data_apache.public_ip}"
+        private_key = "${file(var.privatekeypath)}"
+    }
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum update -y",
+            "sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm",
+            "sudo yum install epel-release-latest-7.noarch.rpm",
+            "sudo yum update -y",
+            "sudo  yum install git python python-devel python-pip ansible -y"
+        ]
+    }
+}
