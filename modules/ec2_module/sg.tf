af9af224540386e6f87ee027b4918297bd5974f9
veerendradevops <veerendratholeti@gmail.com>
2020-06-01 15:52:58 +0530

    added sg

---
+++ modules/ec2_module/sg.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "tf_sg_all" {
+
+name = "tf_sg_apache"
+description = "Allows traffic on port 80"
+vpc_id = "vpc-adc0a0d7"
+ingress{
+    from_port = 80
+    to_port = 80
+    protocol = "tdp"
+    cidr_blocks = ["0.0.0.0/0"]
+} 
+ingress{
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+} 
+egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+}
