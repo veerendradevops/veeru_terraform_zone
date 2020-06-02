cecf5b25c277b56e1cfaab15baf0fba7724e3e97
veerendradevops <veerendratholeti@gmail.com>
2020-06-02 21:07:48 +0530

    added terraform

--- chandra/main.tf
+++ chandra/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "first_ec2" {
     ami = "ami-09d95fab7fff3776c"
     instance_type = "t2.micro"
     key_name = "veeru7799"
-    vpc_security_group_ids = ["sg-0120b8a1ce9608bbc"]
+    vpc_security_group_ids = ["var.securitygroupid"]
     associate_public_ip_address = true
     tags    =   {
         Name = "chandraEC2"
---
+++ chandra/variables.tf
@@ -0,0 +1,3 @@
+variable "securitygroupid" {
+    default = "sg-0120b8a1ce9608bbc"
+}
