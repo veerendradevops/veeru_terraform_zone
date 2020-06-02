10509e02fecab394e5e82222263dc832ff7ccf66
veerendradevops <veerendratholeti@gmail.com>
2020-06-02 18:32:43 +0530

    added terraform

---
+++ workspace/main.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "mainnet" {
+    cidr_block = "192.168.0.0/16"
+
+    tags = {
+        Name = "mainnet"
+    }
+  
+}
+
+resource "aws_subnet" "websubnet" {
+    vpc_id = "${aws_vpc.mainnet.id}"
+    cidr_block = "192.168.0.0/24"
+}
+
+
+resource "aws_subnet" "businesssubnet" {
+    vpc_id = "${aws_vpc.mainnet.id}"
+    cidr_block = "192.168.1.0/24"
+}
+
+
+resource "aws_subnet" "datasubnet" {
+    vpc_id = "${aws_vpc.mainnet.id}"
+    cidr_block = "192.168.2.0/24"
+}
